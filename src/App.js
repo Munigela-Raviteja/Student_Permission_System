7b7a60739d3cefba1ba7eb0ad246599880535966
Munigela-Raviteja <munigelaraviteja@gmail.com>
2023-07-30 19:06:02 +0530

    Student_Permission_System

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Login from "./components/login/Login"
+import Home from "./components//Home/Home"
+import Signup from "./components/signup/Signup"
+import Header from "./components/Header/Header"
+import './App.css';
+import Footer from "./components/Fotter/Fotter";
+
+
+function App() {
+  return (
+    <div className="App">
+    {/* <Login/> */}
+    {/* <Signup/> */}
+    {/* <Home/> */}
+    <Header/>
+   
+    </div>
+  );
+}
+
+export default App;
